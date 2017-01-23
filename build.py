d0d69bc0a28c652b2c83b9f0d01fa36f024316ca
maubarrerag <mauricio.barrerag@gmail.com>
2017-01-22 20:05:53 -0600

    Build python script

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-
+import docker
 import sys
 import argparse
 import json
+from pprint import pprint
 from git import Repo
 from subprocess import call
 
@@ -15,22 +16,28 @@ class Deployer(object):
     def read_info(self, app_name):
     	with open(app_name+'/package.json') as data:
     		myconfig = json.load(data)
-    		self.app_version = myconfig.version
-    		self.app_name = myconfig.app_name
+    		self.app_version = myconfig["version"]
+    		self.app_name = myconfig["name"]
 
     def clone_repo(self, repo, app_name):
     	Repo.clone_from(repo, app_name)
 
-    def build_docker(self):
-    	call(['cd',self.app_name])
-    	call(['docker', 'build', '.'])
-
+    def build_docker(self, user, passw):
+    	client = docker.from_env()
+    	dockerfile = self.app_name+'/'
+    	tag = 'donovosoft/'+self.app_name
+    	image = client.images.build(path=dockerfile, stream=True)
+    	image.tag(tag, self.app_version)
+    	client.login(username=user, password=passw)
+    	for line in client.images.push('docker.io/'+tag, self.app_version, stream=True):
+    		print line
+    	print "OK"
 
 def get_parser():
     parser = argparse.ArgumentParser(description='doNovosoft PAAS build')
     parser.add_argument('-n', '--name', help='Name of the application', required=True)
     parser.add_argument('-r', '--repo', help='Repository end point', required=True)
-    parser.add_argument('-u', '--user', help='Username for registry', required=False)
+    parser.add_argument('-u', '--user', help='Username for registry', required=True)
     parser.add_argument('-p', '--password', help='Password for registry', required=False)
     return parser
 
@@ -40,7 +47,7 @@ def main():
     deploy = Deployer()
     deploy.clone_repo(args.repo, args.name)
     deploy.read_info(args.name)
-    deploy.build_docker()
+    deploy.build_docker(args.user, args.password)
 
 
 if __name__ == '__main__':
